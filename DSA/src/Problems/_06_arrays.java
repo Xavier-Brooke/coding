c1e4cb3c7f41cd53c386b99af9a1abac46f5e012
Xavier Brooke <xavier.brooke.official@gmail.com>
2026-08-04 19:38:00 +0530

    practice : arrays

--- DSA/src/Problems/_06_arrays.java
+++ DSA/src/Problems/_06_arrays.java
@@ -1,10 +1,13 @@
 package Problems;
 
+import static Concept._06_arrays.printArr;
+
 public class _06_arrays {
 
     /*
     * Problem 1 :-
-    * WAF to merge two sorted arrays*/
+    * WAF to merge two sorted arrays
+    * Time Complexity :- O(max(m,n)), Space Complexity :- O(m+n)*/
     public static int[] mergeArr(int[] a, int[] b) {
         int m = a.length ;
         int n = b.length ;
@@ -37,7 +40,8 @@ public class _06_arrays {
 
     /*
     * Problem 2 :-
-    * WAF to sort 0s, 1s and 2s*/
+    * WAF to sort 0s, 1s and 2s
+    * Time Complexity :- O(n), Space Complexity :- O(1)*/
     public static void sort_0s_1s_2s(int[] arr) {
         int i = 1 ;
         int j = arr.length-1 ;
@@ -74,21 +78,70 @@ public class _06_arrays {
         }
     }
 
+    /*
+    * Problem 3 :-
+    * WAF to segregate 0s and 1s
+    * Time Complexity :- O(n), Space Complexity :- O(1)*/
+    public static void segregate0and1(int[] arr) {
+        int n = arr.length ;
+        int si = 0 ;
+        int ei = n-1 ;
+        while(si < ei) {
+            if((arr[si] == 1) && (arr[ei] == 0)) {
+                int temp = arr[si] ;
+                arr[si] = arr[ei] ;
+                arr[ei] = temp ;
+                si++ ;
+                ei-- ;
+            } else if(arr[si] == 0) {
+                si++ ;
+            } else if(arr[ei] == 1) {
+                ei-- ;
+            }
+        }
+    }
 
+    /*
+    * Problem 4 :-
+    * WAF to print wavy array
+    * Time Complexity :- O(n), Space Complexity :- O(1)*/
+    public static void wavyArray(int[] arr) {
+        int n = arr.length ;
+        int i = 0 ;
+
+        while((i < n) && (i != n-1)) {
+            int currIdx = i ;
+            int nexIdx = ++i ;
+            int temp = arr[currIdx] ;
+            arr[currIdx] = arr[nexIdx] ;
+            arr[nexIdx] = temp ;
+            i++ ;
+        }
+    }
 
     // main function
     public static void main(String[] args) {
 
-        // Test Case for Question 2 :-
-        int[] arr = {0, 1, 1, 1, 2, 0, 0, 0, 2} ;
-        sort_0s_1s_2s(arr);
+        // Test Case for Problem 4 :-
+        int[] arr = {2, 4, 7, 8, 9, 10} ;
+        wavyArray(arr);
+        printArr(arr);
 
-        // Test Case for Question 1 :-
+        // Test Case for Problem 3 :-
+//        int[] arr = {0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0} ;
+//        segregate0and1(arr);
+//        printArr(arr) ;
+
+        // Test Case for Problem 2 :-
+//        int[] arr = {0, 1, 1, 1, 2, 0, 0, 0, 2} ;
+//        sort_0s_1s_2s(arr);
+
+        // Test Case for Problem 1 :-
 //        int[] a = {1, 3, 5, 7, 9} ;
 //        int[] b = {2, 4, 6, 8, 10} ;
 //        int[] c = mergeArr(a, b) ;
-        for(int ele : arr) {
-            System.out.print(ele + " ");
-        }
+//        for(int ele : arr) {
+//            System.out.print(ele + " ");
+//        }
     }
 }
