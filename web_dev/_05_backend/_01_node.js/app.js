b2f8824466db496da8014d1e4d75473fe2533b91
Xavier Brooke <xavier.brooke.official@gmail.com>
2026-09-07 02:17:21 +0530

    practice : node.ejs

---
+++ web_dev/_05_backend/_01_node.js/app.js
@@ -0,0 +1,13 @@
+// const figlet = require('figlet') ;
+
+// figlet('Hello node.js', (err, result) => {
+//     if(err) {
+//         console.log('Something went wrong..') ;
+//         console.log(err) ;
+//         return ;
+//     } ;
+//     console.log(result);
+// })
+
+const fruits = require('./fruits') ;
+console.log(fruits.apple) ;
